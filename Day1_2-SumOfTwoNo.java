5e1652865162e10032abad9551241035cbb80220
Akram Ansari <119692799+akram222001@users.noreply.github.com>
2022-12-02 23:34:04 +0530

    Create Day1_2-SumOfTwoNo.java

---
+++ Day1_2-SumOfTwoNo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class SumOfTwoNo 
+{  
+public static void main(String args[])   
+{  
+    Scanner sc = new Scanner(System.in);
+System.out.print("Enter the first number: ");  
+int n1 = sc.nextInt();
+System.out.print("Enter the Second number: ");  
+int n2 = sc.nextInt();  
+int sum = n1 + n2;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
+}  
