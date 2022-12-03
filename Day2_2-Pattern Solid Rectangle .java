458e37cf92afcc672e51c666e10e0caf56ee57ab
Akram Ansari <119692799+akram222001@users.noreply.github.com>
2022-12-03 23:51:27 +0530

    Create Day2_2-Pattern Solid Rectangle .java

---
+++ Day2_2-Pattern Solid Rectangle .java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class pattern {
+public static void main(String args []){
+  Scanner sc=new Scanner(System.in);
+  int x=sc.nextInt();
+  int y=sc.nextInt();
+   for(int i=1; i<=x; i++){
+     for(int j=1; j<=y; j++){
+       System.out.print("*");
+     }
+     System.out.println();
+   }
+ }
+}
+  
+  
