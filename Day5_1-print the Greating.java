fc8b034efdc80d25a8590a5ccff4357f8af4a23f
Akram Ansari <119692799+akram222001@users.noreply.github.com>
2022-12-06 21:05:55 +0530

    Create Day5_1-print the Greating.java

---
+++ Day5_1-print the Greating.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Greating {
+  public static void main(String args []){
+    Scanner sc=new Scanner(System.in);
+    int Bottom=sc.nextInt();
+    if(Bottom==1){
+      System.out.print("hello");
+    }
+    else if(Bottom==2){
+      System.out.print("namaste");
+    }
+    else if(Bottom==3){
+      System.out.print("bounzer");
+    }
+    else{
+    System.out.print("bounzer");
+   }
+  }
+}
